f5958b9a8d52ab47d8b7f80a08dab92d3224326f
Francois Malassenet <malassenet@gmail.com>
2026-03-08 20:43:11 -0400

    version:

--- cmd/simplepatch/main.go
+++ cmd/simplepatch/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/alecthomas/kong"
 	"github.com/fjmalass/simplepatch/internal/cli"
@@ -10,11 +11,27 @@ import (
 	"github.com/fjmalass/simplepatch/internal/log"
 )
 
+// version is set via ldflags at build time
+var version = ""
+
+func getVersion() string {
+	// Prefer ldflags-injected version
+	if version != "" {
+		return version
+	}
+	// Fall back to Go module version (works with go install ...@tag)
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+	return "dev"
+}
+
 func main() {
 	// Instantiate CLI struct in main with global flags
 	var app struct {
-		Verbose bool   `name:"verbose" short:"v" help:"Enable verbose/debug logging"`
-		LogFile string `name:"log" short:"l" help:"Write logs to file"`
+		Version kong.VersionFlag `name:"version" short:"V" help:"Show version"`
+		Verbose bool             `name:"verbose" short:"v" help:"Enable verbose/debug logging"`
+		LogFile string           `name:"log" short:"l" help:"Write logs to file"`
 
 		Patch  cli.PatchCmd  `cmd:"" help:"Apply patches to original files"`
 		Clean  cli.CleanCmd  `cmd:"" help:"Remove backup files"`
@@ -30,6 +47,7 @@ func main() {
 		kong.Name("simplepatch"),
 		kong.Description("Copy patched files to original locations with backup support"),
 		kong.UsageOnError(),
+		kong.Vars{"version": getVersion()},
 		format.KongOption(format.DefaultTheme()),
 	)
 
@@ -52,6 +70,7 @@ func main() {
 	if app.LogFile != "" {
 		logger.Info("logging to file", "path", app.LogFile)
 	}
+	logger.Info("version", "version", getVersion())
 
 	// Bind logger for dependency injection into commands
 	ctx.BindTo(logger, (*log.Logger)(nil))
